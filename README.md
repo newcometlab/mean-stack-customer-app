4e6a50098c844ff0df025f4f3b0282489f34d30f
newcometlab <newcometlab@outlook.com>
2018-08-23 15:04:36 +0200

    Hooked up mongodb to store entries hitting it with a rest client

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # mean-stack-customer-app
+
+    Connection to MongoDB:
+        mongod -dbpath C:\Users\anarc\git-projects\mean-stack-customer-app\data
+    
+    Using "Mongoose" to leverage MongoDB
+    
+    Remember:
+        - you can use "MongoDB Compass" connect: localhost:27017
+        - you need to refresh your Compass to see new entries
+        - use REST client w/ {
+                               "username": "adamski",
+                               "password": "counto",
+                               "email": "cuntomcjunto@gmail.com"
+                             }
+    
+    
--- server.js
+++ server.js
@@ -1,13 +1,34 @@
 var express = require("express");
-var morgan = require("morgan");
-
 var app = express();
 var port = process.env.PORT || 8080;
+var morgan = require("morgan");
+var mongoose = require("mongoose");
+var User = require("./app/models/user");
+var bodyParser = require("body-parser");
 
 app.use(morgan("dev"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+mongoose.connect("mongodb://localhost:27017/home", (error) => {
+    if (error) {
+        console.log("Not connected to database: "+error);
+    } else {
+        console.log("Successfully connected to MongoDB");
+    }
+});
 
 app.get("/home", (req, res) => {
     res.send("Hello from HOME");
+});
+
+app.post("/users", (req, res) => {
+    const user = new User();
+    user.username = req.body.username;
+    user.password = req.body.password;
+    user.email = req.body.email;
+    user.save();
+    res.send("User: "+ user +" successfully created");
 });
 
 app.listen(port, () => {
